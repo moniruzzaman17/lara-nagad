8ac350b4585db645374f085c03f3c88360368125
moniruzzaman17 <moon199715@gmail.com>
2021-08-24 21:07:12 +0600

    updated composer file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a6c097f5d10b3980680dd2a904eb6bfa476646b6',
+    'name' => 'moniruzzaman17/lara-nagad',
+  ),
+  'versions' => 
+  array (
+    'moniruzzaman17/lara-nagad' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a6c097f5d10b3980680dd2a904eb6bfa476646b6',
+    ),
+  ),
+);
